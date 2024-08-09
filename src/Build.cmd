a32a8e5de68ba4f14b6e68150f3b493d79d8471b
Aetopia <41850963+Aetopia@users.noreply.github.com>
2024-08-09 22:10:54 +0530

    Update resources + names.

--- src/Build.cmd
+++ src/Build.cmd
@@ -4,13 +4,17 @@ cd "%~dp0"
 rmdir /Q /S "bin">nul 2>&1
 mkdir "bin">nul 2>&1
 
-windres.exe -i "Resources\.rc" -o "%TEMP%\.o"
+windres.exe -i "Resources\Stonecutter.DirectX.rc" -o "%TEMP%\.o"
 
-gcc.exe -shared -nostdlib -static -s "Stonecutter.DirectX.c" "%SYSTEMROOT%\System32\Kernel32.dll" "%SYSTEMROOT%\System32\ntdll.dll" -lUser32 -ldxgi -ld3d11 -lMinHook -o "bin\Stonecutter.DirectX.dll"
+gcc.exe -shared -nostdlib -static -s "Stonecutter.DirectX.c" "%TEMP%\.o" "%SYSTEMROOT%\System32\Kernel32.dll" "%SYSTEMROOT%\System32\ntdll.dll" -lUser32 -ldxgi -ld3d11 -lMinHook -o "bin\Stonecutter.DirectX.dll"
 
-gcc.exe -mwindows -nostdlib -s "Stonecutter.c" "%TEMP%\.o" -lOle32 -lComctl32 -lKernel32 -lUser32 -lAdvapi32 -o "bin\Stonecutter.exe"
+windres.exe -i "Resources\Stonecutter.Display.rc" -o "%TEMP%\.o"
 
 gcc.exe -mwindows -nostdlib -s "Stonecutter.Display.c" "%TEMP%\.o" -lShell32 -lOle32 -lUser32 -lKernel32 -o "bin\Stonecutter.Display.exe"
+
+windres.exe -i "Resources\Stonecutter.rc" -o "%TEMP%\.o"
+
+gcc.exe -mwindows -nostdlib -s "Stonecutter.c" "%TEMP%\.o" -lOle32 -lComctl32 -lKernel32 -lUser32 -lAdvapi32 -o "bin\Stonecutter.exe"
 
 del "%TEMP%\.o">nul 2>&1
 upx.exe --best --ultra-brute "bin\Stonecutter.DirectX.dll" "bin\Stonecutter.Display.exe" "bin\Stonecutter.exe"
