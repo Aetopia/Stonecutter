8de8b9596b606fd9ac1532a5156d0455053ce24e
Aetopia <41850963+Aetopia@users.noreply.github.com>
2024-10-04 09:12:07 +0530

    Format.

--- src/WinMain.c
+++ src/WinMain.c
@@ -65,6 +65,7 @@ VOID WinMainCRTStartup()
         CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)LoadLibraryW, lpBaseAddress, 0, NULL);
     WaitForSingleObject(hThread, INFINITE);
     VirtualFreeEx(hProcess, lpBaseAddress, 0, MEM_RELEASE);
+    
     CloseHandle(hThread);
     CloseHandle(hProcess);
 
