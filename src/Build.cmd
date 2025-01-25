22354ed5c9d91e1685f21198af1a56f225f3754d
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-01-25 10:17:15 +0530

    Add Shell32 reference.

--- src/Build.cmd
+++ src/Build.cmd
@@ -7,7 +7,7 @@ windres.exe -i "Resources\DllMain.rc" -o "%TEMP%\.o"
 gcc.exe -Os -Wl,--gc-sections -shared -nostdlib -static -s "DllMain.c" "%TEMP%\.o" -lMinHook -lKernel32 -lucrtbase -lUser32 -lDXGI -lD3D11 -o "bin\Stonecutter.dll"
 
 windres.exe -i "Resources\WinMain.rc" -o "%TEMP%\.o"
-gcc.exe -Os -Wl,--gc-sections -mwindows -nostdlib -s "WinMain.c" "%TEMP%\.o" -lShlwapi -lOle32 -lKernel32 -lAdvapi32 -o "bin\Stonecutter.exe"
+gcc.exe -Os -Wl,--gc-sections -mwindows -nostdlib -s "WinMain.c" "%TEMP%\.o" -lShell32 -lShlwapi -lOle32 -lKernel32 -lAdvapi32 -o "bin\Stonecutter.exe"
 
 del "%TEMP%\.o">nul 2>&1
 upx.exe --best --ultra-brute "bin\*">nul 2>&1
