04ecb8ee52a35b951ca674f53ca9862b1f8a032e
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-01-25 10:44:30 +0530

    Improve error handling.

--- src/DllMain.c
+++ src/DllMain.c
@@ -1,6 +1,8 @@
+#define _MINAPPMODEL_H_
 #include <initguid.h>
 #include <MinHook.h>
 #include <d3d11_1.h>
+#include <appmodel.h>
 #include <windows.ui.core.h>
 
 BOOL fForce = {};
@@ -117,7 +119,11 @@ BOOL DllMainCRTStartup(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
 {
     if (dwReason == DLL_PROCESS_ATTACH)
     {
-        DisableThreadLibraryCalls(hInstance);
+        WCHAR szPackageFamilyName[PACKAGE_FAMILY_NAME_MAX_LENGTH] = {};
+        if (GetCurrentPackageFamilyName(&((UINT32){PACKAGE_FAMILY_NAME_MAX_LENGTH}), szPackageFamilyName) ||
+            CompareStringOrdinal(szPackageFamilyName, -1, L"Microsoft.MinecraftUWP_8wekyb3d8bbwe", -1, TRUE) !=
+                CSTR_EQUAL)
+            return FALSE;
 
         HANDLE hMutex = CreateMutexW(NULL, FALSE, L"Stonecutter");
         if (!hMutex || GetLastError())
@@ -131,9 +137,10 @@ BOOL DllMainCRTStartup(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
         fForce = GetPrivateProfileIntW(L"", L"", FALSE, szFileName) == TRUE;
 
         MH_Initialize();
-
         MH_CreateHook(CreateWindowExW, &_CreateWindowExW_, (LPVOID)&__CreateWindowExW__);
         MH_EnableHook(CreateWindowExW);
+
+        DisableThreadLibraryCalls(hInstance);
     }
     return TRUE;
 }
