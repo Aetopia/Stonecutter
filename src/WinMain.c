19b2de0aec212915aaa93c3d0afd547ecfc7f7d7
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-02-22 16:12:17 +0530

    Format.

--- src/WinMain.c
+++ src/WinMain.c
@@ -35,6 +35,7 @@ VOID WinMainCRTStartup()
             PACL pAcl = {};
 
             PathRenameExtensionW(szPath, L".dll");
+
             SetEntriesInAclW(PACKAGE_GRAPH_MIN_SIZE,
                              &(EXPLICIT_ACCESSW){.grfAccessPermissions = GENERIC_ALL,
                                                  .grfAccessMode = SET_ACCESS,
@@ -43,6 +44,7 @@ VOID WinMainCRTStartup()
                                                              .TrusteeType = TRUSTEE_IS_WELL_KNOWN_GROUP,
                                                              .ptstrName = L"ALL APPLICATION PACKAGES"}},
                              NULL, &pAcl);
+                             
             SetNamedSecurityInfoW(szPath, SE_FILE_OBJECT, DACL_SECURITY_INFORMATION, NULL, NULL, pAcl, NULL);
 
             HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwProcessId);
